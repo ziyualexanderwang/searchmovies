5900f3e5b163311fdc0bafb5b7ff5c64d3a4ada4
Xiaofeng Wang <mrwangxiaofeng@gmail.com>
2016-06-05 23:13:25 +0100

    add tv and person search

--- js/app.js
+++ js/app.js
@@ -39,6 +39,10 @@ $(function() {
             if (data.results[i].media_type === "movie") {
               data.results[i].label = data.results[i].title + '  -  ' + data.results[i].media_type;
               data.results[i].value = data.results[i].title + '  -  ' + data.results[i].media_type;
+            } else if (data.results[i].media_type === "tv" || data.results[i].media_type === "person") {
+              data.results[i].label = data.results[i].name + '  -  ' + data.results[i].media_type;
+              data.results[i].value = data.results[i].name + '  -  ' + data.results[i].media_type;
+              data.results[i].title = data.results[i].name;
             }
 
             lists.push(data.results[i]);
@@ -61,6 +65,17 @@ $(function() {
       }
       if (ui.item.media_type === "movie") {
         content += "<p>Release Date: " + ui.item.release_date + "</p>";
+      } else if (ui.item.media_type === "tv") {
+        content += "<p>First Air Date: " + ui.item.first_air_date + "</p>";
+      } else if (ui.item.media_type === "person") {
+        content += "<img style='width:200px;margin:auto;' src='" + configuration.images.secure_base_url + "original/" + ui.item.profile_path + "'></img>";
+        for (var i = 0; i < ui.item.known_for.length; i++) {
+          if (ui.item.known_for[i].media_type === "movie") {
+            content += "<p style='text-align:center;'>Movie: " + ui.item.known_for[i].title + "  (" + ui.item.known_for[i].release_date + ")</p>";
+          } else if (ui.item.known_for[i].media_type === "tv") {
+            content += "<p style='text-align:center;'>TV: " + ui.item.known_for[i].name + "  (" + ui.item.known_for[i].first_air_date + ")</p>";
+          }
+        }
       }
       if (ui.item.overview) {
         content += "<p style='text-align:justify;'>" + ui.item.overview + "</p>";
